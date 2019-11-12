9ffb1bc5bf167ea35d04ae127ffeaecf7e96297d
Paul Sinnett <paul.sinnett@gmail.com>
2019-11-12 01:25:33 +0000

    Initial commit

---
+++ Assets/Materialise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Materialise : MonoBehaviour
+{
+    Material material;
+    float time = 0.0f;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        time = Mathf.Repeat(time + Time.deltaTime, 2.0f);
+        Color color = Color.white;
+        color.a = Mathf.PingPong(time, 1.0f);
+        material.color = color;
+    }
+}
